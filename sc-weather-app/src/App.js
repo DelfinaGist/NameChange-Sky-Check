a13795f5ed565aaca36582fa1aa71fb40a2862cf
DelfinaGist <fullstackcodez@gmail.com>
2023-04-30 21:10:38 -0500

    commit

--- sc-weather-app/src/App.js
+++ sc-weather-app/src/App.js
@@ -1,6 +1,10 @@
 import React, {useState} from 'react';
 import './App.css';
 
+function getWindDirection(deg) {
+  const directions = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW']
+}
+
 function App() {
 
   const apiKey = `32d9f1a0c954f5656882096bc0b1ed1f`;
@@ -21,6 +25,11 @@ function App() {
 
   return (
     <div className="container">
+
+        <div>
+          <p>Welcome to Sky-Check weather app!</p>
+        </div>
+
       <input className="input" placeholder="Type your City here..." 
       onChange={e => setCity(e.target.value)}
       value={city}
@@ -29,7 +38,6 @@ function App() {
 
       {typeof weatherData.main === 'undefined' ?(
         <div>
-          <p>Welcome to Sky-Check weather app!</p>
           <p>Type name of a city to get the weather of.</p>
         </div>  
       ): (
@@ -37,11 +45,18 @@ function App() {
           <p className='city'>{weatherData.name}</p>
           <p className='temp'>{Math.round(weatherData.main.temp)}°F</p>
           <p className='weather'>{weatherData.weather[0].main}</p>
+          <p className='weanter'>{weatherData.main.humidity}% humidity</p>
+          <p className='wind'>Wind: {weatherData.wind.speed} {weatherData.units?.speed} {getWindDirection(weatherData.wind.deg)}</p>
+          {/* <p className='aqi'>AQI: {weatherData.aqi}</p> */}
+          <p className='sunrise-sunset'>Sunrise: {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString()} / Sunset: {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString()} </p>
         </div>  
       )}
 
       {weatherData.cod === "404" ? (
-        <p>City not found.</p>
+        <div>    
+          <h1>Sorry ...</h1>
+          <h3>City not found.</h3>
+        </div>
       ) : (
         <>
         </>
